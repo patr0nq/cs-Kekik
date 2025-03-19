08929afb7d7ad2c01a929db6ad81a3ac76c7f5a6
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-03-20 00:45:42 +0300

    Update HuhuPlugin.kt

--- Huhu/src/main/kotlin/it/dogior/hadEnough/HuhuPlugin.kt
+++ Huhu/src/main/kotlin/it/dogior/hadEnough/HuhuPlugin.kt
@@ -8,30 +8,30 @@ import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 
 @CloudstreamPlugin
 class HuhuPlugin : Plugin() {
-    // Sadece Türkiye'yi içeren sabit bir harita
+    
     private var countries = mapOf(
         "Turkey" to true // Sadece Türkiye aktif
     )
 
-    // Sadece Türkiye'nin dil kodunu içeren harita
+    
     private val countriesToLang = mapOf(
-        "Turkey" to "tr" // Türkiye'nin dil kodu
+        "Turkey" to "tr" 
     )
 
     override fun load(context: Context) {
-        // Preferences
+       
         val sharedPref = context.getSharedPreferences("Huhu", Context.MODE_PRIVATE)
 
-        // Domain'i al (varsayılan olarak "huhu.to")
+       
         val domain = sharedPref.getString("domain", "huhu.to") ?: "huhu.to"
 
-        // Dil kodunu sabit olarak "tr" (Türkçe) yap
+       
         val lang = "tr"
 
-        // Plugin'i kaydet
+        
         registerMainAPI(Huhu(domain, countries, lang))
 
-        // Ayarları etkinleştir
+        
         val activity = context as AppCompatActivity
         openSettings = {
             val frag = Settings(this, sharedPref, countries)
